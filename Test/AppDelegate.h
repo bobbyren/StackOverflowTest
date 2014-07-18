4d730b55ff0f2517ad39727093cec16fbf9b06e5
mitrenegade <bobbyren@gmail.com>
2014-07-18 13:42:39 -0700

    Initial Commit

---
+++ Test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by Bobby Ren on 7/18/14.
+//  Copyright (c) 2014 BobbyRen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Test/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by Bobby Ren on 7/18/14.
+//  Copyright (c) 2014 BobbyRen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
